bdaa2aab291412b38f5dbbe177a9c4f0f00cf86b
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-05-01 10:08:21 -0600

    Project cleanup

---
+++ PlaylistObjC/PlaylistObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlaylistObjC
+//
+//  Created by handje on 5/1/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlaylistObjC/PlaylistObjC/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  PlaylistObjC
+//
+//  Created by handje on 5/1/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
